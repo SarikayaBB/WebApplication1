726da2fad4f6ed2c67de13ef6763fa98ef989af4
bilo <bbesir@hotmail.com>
2022-11-04 20:21:46 +0300

    4.11

---
+++ WebApplication1/Controllers/OrderDetailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        #region db
+        private readonly NorthwindContext _db;
+        public OrderDetailController(NorthwindContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View(_db.OrderDetailsExtendeds.ToList());
+        }
+        public IActionResult GetAllJson()
+        {
+            List<OrderDetailsExtended> list = _db.OrderDetailsExtendeds.ToList<OrderDetailsExtended>();
+
+            return Json(new { data = list });
+        }
+    }
+}
